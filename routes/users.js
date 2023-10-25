fb31c4bbf65678c67cf41d44e67be049282b0836
Mahesh1999-Prog <143574719+Mahesh1999-Prog@users.noreply.github.com>
2023-10-25 16:33:13 -0500

    Interesting Fact

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
+
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+ 
 
+let sum = 0;
+
+let more = 0;
+
+/* GET users listing. */
+
+router.get('/', function(req, res, next) {
+    more += 1;
+    sum += more;
+    res.send(`Sum is: ${sum}`);
+  });
 module.exports = router;
