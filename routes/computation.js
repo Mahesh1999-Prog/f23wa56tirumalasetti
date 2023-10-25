760a7d6ee1c345e7c92a3542e15bd25080dab12b
Mahesh1999-Prog <143574719+Mahesh1999-Prog@users.noreply.github.com>
2023-10-25 16:58:27 -0500

    New Endpoint or Computation

---
+++ routes/computation.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    let x = Math.random() * 100;
+    x = x.toFixed(3)
+    if(req.query.x != undefined){
+        x = parseFloat(req.query.x)
+    }
+    let num1 = Math.atan(x);
+    let num2 = Math.exp(x);
+    let num3 = Math.expm1(x);
+  res.render('computation', {x:x, atan:num1, exp:num2, expm1:num3});
+});
+module.exports = router;
